cb00086b3e4f880e97c821e4f64e3718ecfedd07
Alistair Martin <hello@almartin.com.au>
2024-07-07 23:06:44 +1000

    adds Logo animations

--- src/js/global.js
+++ src/js/global.js
@@ -16,3 +16,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
         $($targetElement).attr("aria-hidden",false);
     });
 });
+
+
+document.querySelectorAll('.brand-switcher--brand path').forEach(path => {
+    const bbox = path.getBBox();
+    const x = bbox.x + bbox.width / 2;
+    const y = bbox.y + bbox.height / 2;
+  
+    // Set CSS variables on each path
+    path.style.setProperty('--origin-x', `${x}px`);
+    path.style.setProperty('--origin-y', `${y}px`);
+});

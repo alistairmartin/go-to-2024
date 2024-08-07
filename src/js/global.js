ab4d1856b02878b0d94be105aa0084eb1b52e80c
Alistair Martin <hello@almartin.com.au>
2024-08-07 20:22:10 +1000

    adds changes

--- src/js/global.js
+++ src/js/global.js
@@ -31,29 +31,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
             targetElement.setAttribute("aria-hidden", false);
         });
     });
-
-    document.querySelector('.product-content-routine').addEventListener("click", function() {
-        var contentRoutine = document.querySelector('.product-content-routine');
-
-        if (contentRoutine.classList.contains('active--morning')) {
-            contentRoutine.classList.add('active--evening');
-            contentRoutine.classList.remove('active--morning');
-        } else {
-            contentRoutine.classList.add('active--morning');
-            contentRoutine.classList.remove('active--evening');
-        }
-    });
-
-    document.querySelector('.product-content-description--teaser-open').addEventListener("click", function() {
-        this.classList.add("hidden");
-        document.querySelector('.product-content-description--teaser-close').classList.remove("hidden");
-        document.querySelector('.product-content-description--teaser').setAttribute('aria-revealed', 'true');
-    });
-
-    document.querySelector('.product-content-description--teaser-close').addEventListener("click", function() {
-        this.classList.add("hidden");
-        document.querySelector('.product-content-description--teaser-open').classList.remove("hidden");
-        document.querySelector('.product-content-description--teaser').setAttribute('aria-revealed', 'false');
-    });
-
 });
